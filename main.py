faa709b7352472a3ac0106f836f8ae2d3147b7e7
Rich <rich@Rich-Precision-M4600.(none)>
2013-04-10 22:30:31 -0400

    more edits

--- main.py
+++ main.py
@@ -25,4 +25,12 @@ f.seek(foo.modelTiepoint[3])
 print '@',f.tell()
 print struct.unpack(H.symbol+'d',f.read(8))
 
+print 'geokey'
+f.seek(foo.geoKey[3])
+print '@',f.tell()
+
+print 'header = ', struct.unpack(H.symbol+'LL',f.read(8))
+for i in xrange(14):
+    print struct.unpack(H.symbol+'hhhh',f.read(8))
+
 f.close()
