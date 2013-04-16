c9037f4f1423c467fad83050e66b126434191ad6
rcrissup <rcrissup@gmail.com>
2013-04-16 15:20:56 -0300

    Update tiff_tools.py
    updates and addition of right,left zero index functions

--- tiff_tools.py
+++ tiff_tools.py
@@ -7,23 +7,19 @@ import binascii
 import struct
 from array import array
 
-class Header(object):
+class _Header(object):
     ''' '''
     version = '0.1'
     
-    def __init__(self, f=None, *args, **kwargs):
+    def __init__(self, f, *args, **kwargs):
         ''' '''
-        if f:
-            self.f = f
+        self.f = f
         self.__curPostion = int()
         self.symbol = str()
         self.ifdOffset = int()
         
-    def get(self,f=None):
+    def get(self):
         ''' '''
-        if f:
-            self.f = f
-        
         self.__curPostion = self.f.tell()
         
         self.f.seek(0)
@@ -37,29 +33,26 @@ class Header(object):
             raise Exception('Not a valid tiff file - failed 42 test')
         
         self.ifdOffset = struct.unpack(self.symbol+'l',self.f.read(4))[0]
-        
-        self.f.seek(self.ifdOffset)
-        #self.numIfds = struct.unpack(self.symbol+'h',self.f.read(2))[0]
-        
+
         self.f.seek(self.__curPostion)
 
 class IFD(object):
     ''' '''
     version = '0.1'
     
-    def __init__(self, f=None, offset=None, *args, **kwargs):
+    def __init__(self, f, offset=None, *args, **kwargs):
         ''' '''
         #Subclass header?
-        
-        if f:
-            self.f = f
-            self.H = Header()
-        self.H.get(self.f)
+        self.f = f
+        self.H = _Header(self.f)
+        self.H.get()
+        self.symbol = self.H.symbol
         
         self.imageWidth = tuple()
         self.imageLength = tuple()
         self.bitsperSample = tuple()
         self.security = tuple()
+        self.resolutionUnit = tuple()
         self.tileWidth = tuple()
         self.tileLength = tuple()
         self.tileOffsets = tuple()
@@ -68,7 +61,12 @@ class IFD(object):
         self.geoKey = tuple()
         self.header = tuple()
         
-    
+        self.tilesAcross = int()
+        self.tilesDown = int()
+        self.tilesPerImage = int()
+        
+        self.pixelWidth = int()
+            
     def get(self, f=None):
         '''  '''
         if f:
@@ -77,13 +75,10 @@ class IFD(object):
         __cur = self.f.tell()
         self.f.seek(self.H.ifdOffset)
         self.numIfds = struct.unpack(self.H.symbol+'h',self.f.read(2))[0]
-        print self.f.tell()
-
+        
         for entry in xrange(self.numIfds):
             data = struct.unpack(self.H.symbol+'HHLL',self.f.read(12))
             tag = data[0]
-            #data = _tag, _type, _count, _offset
-            print data
             
             if tag == 256: #Image Width
                 self.imageWidth = data
@@ -93,6 +88,8 @@ class IFD(object):
                 self.bitsperSample = data
             elif tag == 270: #Security
                 self.security = data
+            elif tag == 296: #Resolution Unit
+                self.resolutionUnit = data
             elif tag == 305: #Software
                 pass
             elif tag == 322: #Tile Width
@@ -109,9 +106,25 @@ class IFD(object):
                 self.geoKey = data
             elif tag == 34737: #NGA Header
                 self.header = data
-            
+        
+        self.tilesAcross = (self.imageWidth[3] + self.tileWidth[3] - 1) / self.tileWidth[3]
+        self.tilesDown = (self.imageLength[3] + self.tileLength[3] -1) / self.tileLength[3]
+        self.tilesPerImage = self.tilesAcross * self.tilesDown
         
         self.f.seek(__cur)
+
+def gettileoffsets(f, obj):
+    '''
+    f = file
+    obj = IFD.tileoffsers tuple
+    returns array of tile offsets
+    '''
+    offsets = array('l')
+    cur = f.tell()
+    f.seek(obj[3])
+    offsets.read(f, obj[2])
+    f.seek(cur)
+    return offsets.tolist()
     
 def gettile(f,length,width,offset):
     '''
@@ -129,5 +142,29 @@ def gettile(f,length,width,offset):
         scanline += 1
     f.seek(cur)
     return tile
+
+def leftzeroindex(_list):
+    '''
+    find the leftmost position of valid pixels
+    returns None if all values in _list are 0
+    '''
+    index = 0
+    for pixel in _list:
+        if pixel != 0:
+            return index
+        else:
+            index += 1
+
+def rightzeroindex(_list):
+    '''
+    finds the right most position of valid pixels
+    returns None if all values in _list are 0
+    '''
+    index = len(_list) - 1
+    while index > 0:
+        if _list[index] != 0:
+            return index
+        else:
+            index -= 1
 
         
